802e2039a46bfb6ce81f3844fd2e39008dd13522
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-12 19:26:12 +0530

    Program 028: Singly Linked List - Demonstrating Yoda Condition Error Prevention

---
+++ C/Linked List/Singly Linked List/SinglyLL028.c
@@ -0,0 +1,26 @@
+/*
+=====================================================================
+
+Program Name    : Singly Linked List - Yoda Condition Demonstration
+Program Number  : 028
+Description     : This program demonstrates a defensive programming
+                  technique known as Yoda Condition. By placing the
+                  constant value on the left side of a comparison,
+                  accidental assignment operations can be detected
+                  by the compiler during compilation.
+
+Concepts Used   : Structure, Self-Referential Structure,
+                  typedef, Dynamic Memory Allocation,
+                  Pointer Manipulation, NULL Comparison,
+                  Defensive Programming, Compiler Error Detection
+
+Input           : None
+
+Output          : Compilation Error
+
+Date            : July 2026
+Language        : C
+Author          : Atharv Tushar Bhosale
+
+=====================================================================
+*/
