29a71cde021e7549f2d38cdb420f6756a4e59d92
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-13 21:01:47 +0530

    Program 010: Implemented InsertFirst in C++ Singly Linked List  Added dynamic node creation using new operator. Implemented InsertFirst() member function. Maintained node count using iCount variable. Displayed linked list and total element count. Demonstrated insertion at beginning of the list.

--- C++/Linked List/Singly Linked List/SinglyLL010.cpp
+++ C++/Linked List/Singly Linked List/SinglyLL010.cpp
@@ -2,25 +2,29 @@
 =====================================================================
 
 Program Name    : Singly Linked List - InsertFirst Operation (C++)
-Program Number  : 006
+Program Number  : 010
 Description     : This program demonstrates the implementation
                   of the InsertFirst() member function in a
-                  Singly Linked List using C++ classes.
+                  Singly Linked List using a C++ class.
 
-                  New nodes are dynamically created using
+                  New nodes are dynamically allocated using
                   the new operator and inserted at the
                   beginning of the linked list.
 
+                  The program also maintains the total
+                  number of nodes using the iCount variable.
+
 Concepts Used   : C++, Classes, Constructor,
                   Dynamic Memory Allocation,
-                  Encapsulation,
                   Self-Referential Structure,
-                  InsertFirst Operation
+                  Encapsulation,
+                  InsertFirst Operation,
+                  Node Counting
 
 Input           : Node values (11, 21, 51)
 
-Output          : Displays the linked list and
-                  total number of nodes.
+Output          : Displays the linked list after insertion
+                  and prints the total number of nodes.
 
 Date            : July 2026
 Language        : C++
@@ -29,36 +33,130 @@ Author          : Atharv Tushar Bhosale
 =====================================================================
 */
 
-/*
-=====================================================================
+#include<iostream>
+using namespace std;
 
-Program Name    : Singly Linked List - InsertFirst Operation (C++)
-Program Number  : 006
-Description     : This program demonstrates the implementation
-                  of the InsertFirst() member function in a
-                  Singly Linked List using C++ classes.
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
 
-                  New nodes are dynamically created using
-                  the new operator and inserted at the
-                  beginning of the linked list.
+typedef struct node NODE;
+typedef struct node *PNODE;
 
-Concepts Used   : C++, Classes, Constructor,
-                  Dynamic Memory Allocation,
-                  Encapsulation,
-                  Self-Referential Structure,
-                  InsertFirst Operation
+class SinglyLL
+{
+    private:
+        PNODE first;
+        int iCount;
 
-Input           : Node values (11, 21, 51)
+    public:
+        SinglyLL();
+        void Display();
+        int Count();
+        void InsertFirst(int iNo);
+        void InsertLast(int iNo);
+        void InsertAtPos(int iNo, int iPos);
+        void DeleteFirst();
+        void DeleteLast();
+        void DeleteAtPos(int iPos);
+};
 
-Output          : Displays the linked list and
-                  total number of nodes.
+SinglyLL::SinglyLL()
+{
+    this -> first = NULL;
+    this -> iCount = 0;
+}
 
-Date            : July 2026
-Language        : C++
-Author          : Atharv Tushar Bhosale
+void SinglyLL:: Display()
+{
+    PNODE temp = NULL;
+    temp = this -> first;
 
-=====================================================================
-*/
+    while(temp != NULL)
+    {
+        cout<<"| "<<temp -> data<<" |-> ";
+        temp = temp -> next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+int SinglyLL:: Count()
+{
+    return this -> iCount;
+}
+
+void  SinglyLL:: InsertFirst(int iNo)
+{
+    PNODE newn = NULL;
+
+    newn = new NODE;
+
+    newn -> data = iNo;
+    newn -> next = NULL;
+
+    if(this -> first == NULL)
+    {
+        this -> first = newn;
+    }
+    else
+    {
+        newn -> next = this -> first;
+        this -> first = newn;
+    }
+    this -> iCount++;                               // Important
+}
+
+void  SinglyLL:: InsertLast(int iNo)
+{
+    PNODE newn = NULL;
+
+    newn = new NODE;
+
+    newn -> data = iNo;
+    newn -> next = NULL;
+
+    if(this -> first == NULL)
+    {
+        this -> first = newn;
+    }
+    else
+    {
+        
+    }
+    this -> iCount++;                              // Important
+}
+
+void  SinglyLL:: InsertAtPos(int iNo, int iPos)
+{}
+
+void  SinglyLL:: DeleteFirst()
+{}
+
+void  SinglyLL:: DeleteLast()
+{}
+
+void  SinglyLL:: DeleteAtPos(int iPos)
+{}
+
+int main()
+{
+    SinglyLL sobj;
+    int iRet = 0;
+
+    sobj.InsertFirst(51);                // InsertFirst(sobj,51)
+    sobj.InsertFirst(21);                // InsertFirst(sobj,21)
+    sobj.InsertFirst(11);                // InsertFirst(sobj,11)
+
+    sobj.Display();
+
+    iRet = sobj.Count();
+    cout<<"Number of Elements are: "<<iRet<<endl;
+
+    return 0;
+}
 
 /*
 =====================================================================
@@ -70,62 +168,92 @@ Number of Elements are: 3
 
 Explanation :
 
-Initially the linked list is empty.
+Initially :
 
-Insertion Sequence :
+first  = NULL
+iCount = 0
+
+--------------------------------------------------
 
 InsertFirst(51)
 
 first
-  |
-  v
-+----+------+ 
+ |
+ v
++----+------+
 | 51 | NULL |
 +----+------+
+
+iCount = 1
+
+--------------------------------------------------
 
 InsertFirst(21)
 
 first
-  |
-  v
+ |
+ v
 +----+------+    +----+------+
 | 21 |   o------>| 51 | NULL |
 +----+------+    +----+------+
 
+iCount = 2
+
+--------------------------------------------------
+
 InsertFirst(11)
 
 first
-  |
-  v
+ |
+ v
 +----+------+    +----+------+    +----+------+
 | 11 |   o------>| 21 |   o------>| 51 | NULL |
 +----+------+    +----+------+    +----+------+
+
+iCount = 3
+
+--------------------------------------------------
 
 Display Traversal :
 
 11 -> 21 -> 51 -> NULL
 
-Count :
+--------------------------------------------------
+
+Count Function :
+
+The Count() function returns the value of iCount.
 
 iCount = 3
 
+--------------------------------------------------
+
 Operations Performed :
 
-1. Created Singly Linked List object.
-2. Initialized first to NULL.
-3. Inserted node 51 at first position.
-4. Inserted node 21 at first position.
-5. Inserted node 11 at first position.
-6. Incremented iCount after every insertion.
-7. Displayed all nodes.
-8. Returned total node count.
+1. Created SinglyLL object.
+2. Constructor initialized first to NULL.
+3. Constructor initialized iCount to 0.
+4. Inserted node 51 at first position.
+5. Inserted node 21 at first position.
+6. Inserted node 11 at first position.
+7. Incremented iCount after every insertion.
+8. Displayed all nodes.
+9. Returned total node count.
+
+Time Complexity :
+
+InsertFirst() : O(1)
+Display()     : O(N)
+Count()       : O(1)
 
 Conclusion :
 
-InsertFirst() inserts a new node at the beginning
-of the linked list in constant time O(1).
-The newly inserted node always becomes the first node
-of the linked list.
+InsertFirst() is one of the most efficient
+linked list operations because insertion
+occurs directly at the beginning of the list.
+
+No traversal is required, resulting in
+constant time complexity O(1).
 
 =====================================================================
 */
