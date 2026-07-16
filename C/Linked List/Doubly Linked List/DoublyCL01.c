e0093fc25ac9467327e9275a1ec6e2e59e6af5ac
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-16 22:09:55 +0530

    Added Program 001 - Doubly Linked List Node Structure  - Created self-referential doubly linked list node - Added next and prev pointers - Demonstrated structure packing using #pragma pack(1) - Displayed size of NODE structure using sizeof()

---
+++ C/Linked List/Doubly Linked List/DoublyCL01.c
@@ -0,0 +1,28 @@
+/*
+=====================================================================
+
+Name            : Doubly Linked List - Node Structure Size
+Program Number  : 001
+Description     : This program demonstrates the basic structure of a
+                  Doubly Linked List node. Each node contains:
+                  - Data field
+                  - Pointer to next node
+                  - Pointer to previous node
+
+                  The program displays the total size occupied by
+                  the node structure in memory using structure packing.
+
+Concepts Used   : Doubly Linked List, Self-Referential Structure,
+                  Structure Packing, Pointers, Memory Layout,
+                  sizeof Operator
+
+Input           : None
+
+Output          : Size of Doubly Linked List node structure
+
+Date            : July 2026
+Language        : C
+Author          : Atharv Tushar Bhosale
+
+=====================================================================
+*/
