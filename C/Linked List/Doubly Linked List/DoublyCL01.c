ec853aae4b595172cd2e49a629de8d1be814bfbe
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-17 11:09:13 +0530

    Added Program 001 - Doubly Linked List Node Structure Size  - Created basic Doubly Linked List node structure - Added next and prev self-referential pointers - Demonstrated structure packing using #pragma pack(1) - Displayed memory size of NODE using sizeof()

--- C/Linked List/Doubly Linked List/DoublyCL01.c
+++ C/Linked List/Doubly Linked List/DoublyCL01.c
@@ -25,4 +25,57 @@ Language        : C
 Author          : Atharv Tushar Bhosale
 
 =====================================================================
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+int main()
+{
+    printf("Size of NODE is : %d\n",sizeof(NODE));
+
+    return 0;
+}
+
+/*
+=====================================================================
+
+Sample Output :
+
+Size of NODE is : 20
+
+=====================================================================
+
+Memory Layout :
+
++--------+---------+---------+
+|  data  |  next   |  prev   |
++--------+---------+---------+
+
+=====================================================================
+
+Note :
+
+- Doubly Linked List node contains two pointers.
+- next pointer stores address of next node.
+- prev pointer stores address of previous node.
+- #pragma pack(1) removes compiler-added padding.
+- sizeof() is used to determine total memory occupied
+  by the structure.
+
+=====================================================================
 */
