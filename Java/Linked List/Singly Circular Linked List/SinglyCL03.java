eab2fd80133484a839c3f53da87ae9b3455c3d51
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-18 16:48:55 +0530

    Added Program 003 - Node Constructor Demonstration  - Created parameterized constructor - Initialized node data using constructor - Set next reference to null - Demonstrated object initialization

---
+++ Java/Linked List/Singly Circular Linked List/SinglyCL03.java
@@ -0,0 +1,68 @@
+/*
+=====================================================================
+
+Name            : Singly Linked List - Node Constructor
+Program Number  : 005
+Description     : This program demonstrates the use of a
+                  parameterized constructor in a node class.
+
+                  The constructor initializes:
+                  - Data field
+                  - Next reference
+
+Concepts Used   : Constructor, Object Initialization,
+                  this Keyword, Node Creation
+
+Input           : Integer value passed to constructor
+
+Output          : Data and next reference value
+
+Date            : July 2026
+Language        : Java
+Author          : Atharv Tushar Bhosale
+
+=====================================================================
+*/
+
+class node
+{
+    public int data;
+    public node next;
+
+    node(int no)
+    {
+        this.data = no;
+        this.next = null;
+    }
+}
+
+class SinglyCL03
+{
+    public static void main(String A[])
+    {
+        node newn = new node(11);
+
+        System.out.println(newn.data);
+        System.out.println(newn.next);
+    }
+}
+
+/*
+=====================================================================
+
+Sample Output :
+
+11
+null
+
+=====================================================================
+
+Observation :
+
+- Constructor initializes node automatically.
+- Data is assigned during object creation.
+- Next reference is initialized to null.
+- Reduces manual initialization code.
+
+=====================================================================
+*/
