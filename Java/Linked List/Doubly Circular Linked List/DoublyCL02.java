de46140ea99147a0341b31a9277f99ea53a9a74b
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-30 15:07:29 +0530

    Added Program 002: Implemented InsertFirst operation in Doubly Circular Linked List using Java

---
+++ Java/Linked List/Doubly Circular Linked List/DoublyCL02.java
@@ -0,0 +1,89 @@
+/*
+===============================================================================
+Program Name    : Doubly Circular Linked List - InsertFirst
+Program Number  : 002
+File Name       : DoublyCL02.java
+
+Description     : This program demonstrates the InsertFirst()
+                  operation in a Doubly Circular Linked List
+                  using Java.
+
+Date            : July 2026
+Language        : Java
+Author          : Atharv Tushar Bhosale
+===============================================================================
+*/
+
+class node
+{
+    public int data;
+    public node next;
+    public node prev;
+
+    node(int no)
+    {
+        this.data = no;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyCL
+{
+    private node first;
+    private node last;
+    private int iCount;
+
+    public DoublyCL()
+    {
+        first = null;
+        last = null;
+        iCount = 0;
+    }
+
+    public void InsertFirst(int iNo)
+    {
+        node newn = new node(iNo);
+
+        if(first == null && last == null)
+        {
+            first = newn;
+            last = newn;
+        }
+        else
+        {
+            newn.next = first;
+            first.prev = newn;
+            first = newn;
+        }
+
+        last.next = first;
+        first.prev = last;
+
+        iCount++;
+    }
+}
+
+class DoublyCL02
+{
+    public static void main(String A[])
+    {
+        DoublyCL dobj = new DoublyCL();
+
+        dobj.InsertFirst(51);
+        dobj.InsertFirst(21);
+        dobj.InsertFirst(11);
+
+    }
+}
+
+/*
+===============================================================================
+Conclusion:
+===============================================================================
+
+Program 002 demonstrates insertion of nodes at the beginning
+of a Doubly Circular Linked List using Java.
+
+===============================================================================
+*/ 
