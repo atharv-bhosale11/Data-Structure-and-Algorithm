b516cf7858108fd3433855b4006cd3e3e63a2e09
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-02 21:33:49 +0530

    Added Program 004 - Implemented basic Stack using Singly Linear Linked List in C++  - Created Stack class - Implemented Push operation - Implemented Display operation - Added Count functionality - Used dynamic memory allocation - Demonstrated Stack insertion and traversal

---
+++ C++/Stack/Stack_04.cpp
@@ -0,0 +1,124 @@
+////////////////////////////////////////////////////////////
+//
+//  Program Name    : Stack Using Singly Linear Linked List
+//  Program Number  : 004
+//  Description     : Implementation of Stack Data Structure
+//                    using Singly Linear Linked List.
+//
+//                    Operations Implemented:
+//                    1. Push
+//                    2. Display
+//                    3. Count
+//
+//  Concepts Used   : Class, Object, Linked List,
+//                    Dynamic Memory Allocation, Stack
+//
+//  Language        : C++
+//
+//  Author          : Atharv Tushar Bhosale
+//  Date            : August 2026
+//
+////////////////////////////////////////////////////////////
+
+// Stack 
+
+#include<iostream>
+
+using namespace std;
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+class Stack
+{
+    private:
+        struct node* first;
+        int iCount;
+
+    public:
+        Stack();
+        void Push(int iNo);             // Insert First
+        int Pop();                     // Delete First
+        int Peep();                    // Delete First (it will give data only not deleting data)
+        void Display();
+        int Count();
+};
+
+Stack :: Stack()
+{
+    this -> first = NULL;
+    this -> iCount = 0;
+}
+
+void Stack :: Push(int iNo)           
+{
+    struct node* newn = NULL;
+
+    newn = new struct node();
+    newn -> data = iNo;
+    newn -> next = NULL;
+    
+    newn -> next = first;
+    first = newn;
+
+    iCount++;
+}
+
+int Stack :: Pop()
+{
+    return 0;
+}           
+
+int Stack :: Peep()   
+{}
+
+void Stack :: Display()
+{
+    struct node* temp = NULL;
+
+    temp = first;
+
+    while(temp != NULL)
+    {
+        cout<<"| "<<temp->data<<" |\n";
+        temp = temp -> next;
+    }
+}
+
+int Stack :: Count()
+{
+    return iCount;
+}
+
+int main()
+{
+    Stack sobj;
+    int iRet = 0;
+
+    sobj.Push(11);
+    sobj.Push(21);
+    sobj.Push(51);
+    sobj.Push(101);
+
+    iRet = sobj.Count();
+    cout<<"Elements of this stack:"<<iRet<<endl;
+
+    sobj.Display();
+
+    return 0;
+}
+
+////////////////////////////////////////////////////////////
+//
+//  End of Program
+//
+//  Stack Using Singly Linear Linked List
+//
+//  Author : Atharv Tushar Bhosale
+//  Date   : August 2026
+//
+////////////////////////////////////////////////////////////
