8f96d02107eade28c945a44ae904318044f0e4b5
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-05 09:42:27 +0530

    Added Program 003 : Added Display Operation in Queue

---
+++ C/Queue/Queue_03.c
@@ -0,0 +1,90 @@
+/*
+===============================================================================
+Program Name    : Queue Display Operation
+Program Number  : 003
+File Name       : Queue03.c
+
+Description     : This program demonstrates Display
+                  operation in Queue.
+
+Date            : August 2026
+Language        : C
+Author          : Atharv Tushar Bhosale
+===============================================================================
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void Enqueue(PPNODE first,int iNo)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("|%d| -> ",first->data);
+        first = first->next;
+    }
+
+    printf("NULL\n");
+}
+
+int main()
+{
+    PNODE head = NULL;
+
+    Enqueue(&head,11);
+    Enqueue(&head,21);
+    Enqueue(&head,51);
+    Enqueue(&head,101);
+
+    Display(head);
+
+    return 0;
+}
+
+/*
+===============================================================================
+Output :
+
+|11| -> |21| -> |51| -> |101| -> NULL
+
+===============================================================================
+End of Program
+===============================================================================
+*/
