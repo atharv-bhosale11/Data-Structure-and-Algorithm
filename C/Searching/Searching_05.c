b524d70c714769c85163184c1bb1153a4f132eb7
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-06 20:03:45 +0530

    Added Program 005 : Completed Searching Techniques program with Linear Search and Bi-Directional Search in C

---
+++ C/Searching/Searching_05.c
@@ -0,0 +1,186 @@
+/*
+===============================================================================
+Program Name    : Searching Techniques
+Program Number  : 005
+File Name       : Searching05.c
+
+Description     : This program demonstrates Searching
+                  Techniques using Array.
+                  
+                  Operations:
+                  1. Linear Search
+                  2. Bi-Directional Search
+
+Date            : August 2026
+Language        : C
+Author          : Atharv Tushar Bhosale
+===============================================================================
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Searching
+{
+    int *Arr;
+    int iSize;
+};
+
+typedef struct Searching SEARCHING;
+typedef struct Searching* PSEARCHING;
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : Accept
+// Description   : Accept elements from user
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void Accept(PSEARCHING first)
+{
+    int i = 0;
+
+    printf("Enter the Elements:\n");
+
+    for(i = 0; i < first->iSize; i++)
+    {
+        scanf("%d",&first->Arr[i]);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : Display
+// Description   : Display elements of array
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void Display(PSEARCHING first)
+{
+    int i = 0;
+
+    printf("Elements of the Array are:\n");
+
+    for(i = 0; i < first->iSize; i++)
+    {
+        printf("%d\n",first->Arr[i]);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : LinearSearch
+// Description   : Search element using Linear Search
+//
+///////////////////////////////////////////////////////////////////////////////
+
+int LinearSearch(PSEARCHING first,int iNo)
+{
+    int i = 0;
+
+    for(i = 0; i < first->iSize; i++)
+    {
+        if(first->Arr[i] == iNo)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : BiDirectionalSearch
+// Description   : Search element using Bi-Directional Search
+//
+///////////////////////////////////////////////////////////////////////////////
+
+int BiDirectionalSearch(PSEARCHING first,int iNo)
+{
+    int iStart = 0;
+    int iEnd = first->iSize - 1;
+
+    while(iStart <= iEnd)
+    {
+        if((first->Arr[iStart] == iNo) || (first->Arr[iEnd] == iNo))
+        {
+            return 1;
+        }
+
+        iStart++;
+        iEnd--;
+    }
+
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Entry Point Function
+//
+///////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    SEARCHING sobj;
+    int iRet = 0;
+
+    sobj.iSize = 5;
+    sobj.Arr = (int *)malloc(sizeof(int) * sobj.iSize);
+
+    Accept(&sobj);
+    Display(&sobj);
+
+    iRet = LinearSearch(&sobj,30);
+
+    if(iRet == 1)
+    {
+        printf("Element is Present using Linear Search\n");
+    }
+    else
+    {
+        printf("There is no such element using Linear Search\n");
+    }
+
+    iRet = BiDirectionalSearch(&sobj,30);
+
+    if(iRet == 1)
+    {
+        printf("Element is Present using Bi-Directional Search\n");
+    }
+    else
+    {
+        printf("There is no such element using Bi-Directional Search\n");
+    }
+
+    free(sobj.Arr);
+
+    return 0;
+}
+
+/*
+===============================================================================
+Sample Output
+
+Enter the Elements:
+10
+20
+30
+40
+50
+
+Elements of the Array are:
+10
+20
+30
+40
+50
+
+Element is Present using Linear Search
+Element is Present using Bi-Directional Search
+
+===============================================================================
+End of Program
+===============================================================================
+*/
