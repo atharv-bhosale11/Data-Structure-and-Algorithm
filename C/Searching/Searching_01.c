05e37e1381b934f1e3dc8d3dd0aa9d7c1f6fefc0
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-06 19:58:30 +0530

    Added Program 01: Searching Skeleton in C with dynamic memory allocation structure

---
+++ C/Searching/Searching_01.c
@@ -0,0 +1,59 @@
+/*
+===============================================================================
+Program Name    : Searching Skeleton
+Program Number  : 001
+File Name       : Searching01.c
+
+Description     : Basic Skeleton Program for Searching Techniques.
+
+Date            : August 2026
+Language        : C
+Author          : Atharv Tushar Bhosale
+===============================================================================
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Searching
+{
+    int *Arr;
+    int iSize;
+};
+
+typedef struct Searching SEARCHING;
+typedef struct Searching* PSEARCHING;
+
+void Accept(PSEARCHING ptr)
+{
+}
+
+void Display(PSEARCHING ptr)
+{
+}
+
+int main()
+{
+    SEARCHING sobj;
+
+    sobj.iSize = 5;
+    sobj.Arr = (int *)malloc(sizeof(int) * sobj.iSize);
+
+    Accept(&sobj);
+    Display(&sobj);
+
+    free(sobj.Arr);
+
+    return 0;
+}
+
+/*
+===============================================================================
+Output :
+
+(No Output)
+
+===============================================================================
+End of Program
+===============================================================================
+*/
