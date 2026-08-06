21df934d259e519ffb19c380c8c65fe92b1cea28
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-06 20:08:31 +0530

    Added Program 003: Linear Search using Array in C++

---
+++ C++/Searching/Searching_03.cpp
@@ -0,0 +1,168 @@
+/*
+===============================================================================
+Program Name    : Linear Search
+Program Number  : 003
+File Name       : Searching03.cpp
+
+Description     : This program accepts elements from user,
+                  displays them and performs Linear Search.
+
+Date            : August 2026
+Language        : C++
+Author          : Atharv Tushar Bhosale
+===============================================================================
+*/
+
+#include<iostream>
+using namespace std;
+
+class Searching
+{
+    private:
+        int *Arr;
+        int iSize;
+
+    public:
+        Searching(int iNo);
+        ~Searching();
+
+        void Accept();
+        void Display();
+
+        bool LinearSearch(int iNo);
+};
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Constructor
+//
+///////////////////////////////////////////////////////////////////////////////
+
+Searching :: Searching(int iNo)
+{
+    iSize = iNo;
+    Arr = new int[iSize];
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Destructor
+//
+///////////////////////////////////////////////////////////////////////////////
+
+Searching :: ~Searching()
+{
+    delete []Arr;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : Accept
+// Description   : Accept elements from user
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void Searching :: Accept()
+{
+    int i = 0;
+
+    cout<<"Enter the Elements:\n";
+
+    for(i = 0; i < iSize; i++)
+    {
+        cin>>Arr[i];
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : Display
+// Description   : Display elements of array
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void Searching :: Display()
+{
+    int i = 0;
+
+    cout<<"Elements of the Array are:\n";
+
+    for(i = 0; i < iSize; i++)
+    {
+        cout<<Arr[i]<<"\n";
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : LinearSearch
+// Description   : Search element using Linear Search
+//
+///////////////////////////////////////////////////////////////////////////////
+
+bool Searching :: LinearSearch(int iNo)
+{
+    bool bFlag = false;
+    int i = 0;
+
+    for(i = 0; i < iSize; i++)
+    {
+        if(Arr[i] == iNo)
+        {
+            bFlag = true;
+            break;
+        }
+    }
+
+    return bFlag;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Entry Point Function
+//
+///////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    Searching sobj(5);
+
+    sobj.Accept();
+    sobj.Display();
+
+    if(sobj.LinearSearch(30) == true)
+    {
+        cout<<"Element is Present\n";
+    }
+    else
+    {
+        cout<<"There is no such element\n";
+    }
+
+    return 0;
+}
+
+/*
+===============================================================================
+Sample Output
+
+Enter the Elements:
+10
+20
+30
+40
+50
+
+Elements of the Array are:
+10
+20
+30
+40
+50
+
+Element is Present
+
+===============================================================================
+End of Program
+===============================================================================
+*/
