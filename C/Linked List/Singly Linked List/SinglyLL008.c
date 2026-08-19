113c307ffd360a06a99a48af2a1a14b8962ebc62
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-19 21:02:10 +0530

    Added Program 008 : Implement Singly Linked List using typedef  - Created self-referential structure for linked list node - Used typedef to define NODE alias for struct node - Declared node objects and pointers using NODE - Created head pointer and linked three nodes manually

--- C/Linked List/Singly Linked List/SinglyLL008.c
+++ C/Linked List/Singly Linked List/SinglyLL008.c
@@ -4,7 +4,7 @@
 Program Name    : Singly Linked List - Using typedef for Node
 Program Number  : 008 
 Description     : This program demonstrates the use of typedef to
-                  create an alias for a self-referential structure.
+                  create an alias for a self-referential structure. 
                   It simplifies the declaration of node objects and
                   pointers, making linked list programs easier to
                   read and maintain.
