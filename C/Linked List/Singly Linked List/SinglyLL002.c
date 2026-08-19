c4e4c58df85a58f028e316e32701981a9492dc5c
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-19 20:58:08 +0530

    Added Program 002 : Singly Linked List - Structure Packing Implemented structure packing using #pragma pack(1) to remove compiler-added padding from the node structure. Analyzed memory optimization techniques and compared packed versus default structure layouts.

--- C/Linked List/Singly Linked List/SinglyLL002.c
+++ C/Linked List/Singly Linked List/SinglyLL002.c
@@ -3,7 +3,7 @@
 
 Program Name    : Singly Linked List - Structure Packing
 Program Number  : 002
-Description     : This program demonstrates the use of #pragma pack(1)
+Description     : This program demonstrates the use of #pragma pack(1) 
                   to remove compiler-added padding from a singly linked 
                   list node structure. It illustrates how structure 
                   packing affects memory consumption and structure size.
