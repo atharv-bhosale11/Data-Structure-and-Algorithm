1f094b3ae2547c929561fd7483c9e2b14ee9bb11
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-18 10:58:58 +0530

    Added Program 006 : Singly Linked List - Traversal Using While Loop  - Created Singly Linked List with three nodes - Connected nodes using next references - Initialized Head pointer - Traversed Linked List using while loop - Displayed all node elements sequentially

--- Java/Linked List/Singly Linked List/SinglyLL006.java
+++ Java/Linked List/Singly Linked List/SinglyLL006.java
@@ -1,7 +1,7 @@
 /*
 =====================================================================
 
-Name            : Singly Linked List - Traversal Using While Loop
+Name            : Singly Linked List - Traversal Using While Loop 
 Program Number  : 006 
 
 Description     : This program demonstrates how to traverse a
