1253d2b0b10cad4f2d7baf47f139e05f92f6bdb4
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-21 16:18:09 +0530

    Added Program 010 : Singly Linked List - InsertFirst with Node Counter  - Implemented InsertFirst operation - Added node count maintenance - Demonstrated head pointer manipulation - Added constructor-based initialization - Inserted nodes at beginning of linked list main

--- Java/Linked List/Singly Linked List/SinglyLL010.java
+++ Java/Linked List/Singly Linked List/SinglyLL010.java
@@ -4,7 +4,7 @@
 Name            : Singly Linked List - InsertFirst with Node Counter
 Program Number  : 010
  
-Description     : This program demonstrates the InsertFirst()
+Description     : This program demonstrates the InsertFirst() 
                   operation in a Singly Linked List along with
                   maintaining a node count. Each insertion at
                   the beginning updates the head pointer and
