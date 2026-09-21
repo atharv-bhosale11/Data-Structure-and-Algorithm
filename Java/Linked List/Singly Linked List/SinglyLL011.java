7307755da472bd6e55087415cd4707080448299c
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-21 16:18:47 +0530

    Added Program 011 : Singly Linked List - InsertLast Operation  - Implemented InsertLast functionality - Added traversal to reach last node - Linked new node at end of list - Maintained node count after insertion - Demonstrated insertion at both ends

--- Java/Linked List/Singly Linked List/SinglyLL011.java
+++ Java/Linked List/Singly Linked List/SinglyLL011.java
@@ -4,7 +4,7 @@
 Name            : Singly Linked List - InsertLast Operation
 Program Number  : 011
  
-Description     : This program demonstrates insertion of nodes
+Description     : This program demonstrates insertion of nodes 
                   at both the beginning and end of a Singly
                   Linked List. The InsertLast() function
                   traverses the list and attaches the new node
